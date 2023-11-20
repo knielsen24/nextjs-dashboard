79665a74c03be036cd3c9248f82fe9463ea2a459
kevinnielsen <knielsen24@gmail.com>
2023-11-20 10:27:06 -0800

    added alt to image

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -31,7 +31,7 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt={"Customer's avatar"}
+                        alt="Customer's avatar"
                       />
                       <p>{invoice.name}</p>
                     </div>
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+						alt="Customer's avatar"
                       />
                       <p>{invoice.name}</p>
                     </div>
